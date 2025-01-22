5c5be8cf104a1435acbca16a0e904e9522459c22
yensen-arch <officialdevain25@gmail.com>
2025-01-22 21:59:02 +0530

    LP

--- src/Routes/Homepage.jsx
+++ src/Routes/Homepage.jsx
@@ -2,22 +2,21 @@
 
 import { useState } from "react"
 import { Swiper, SwiperSlide } from "swiper/react"
-import SwiperCore, { Mousewheel, Pagination } from "swiper"
+import SwiperCore, { Mousewheel } from "swiper"
 import "swiper/css"
-import "swiper/css/pagination"
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos"
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos"
 import { Link } from "react-router-dom"
 import Navbar from "../Components/Navbar"
 import FooterLP from "../Components/FooterLP"
 
-SwiperCore.use([Mousewheel, Pagination])
+SwiperCore.use([Mousewheel])
 
 const Homepage = () => {
   const Women = [
     {
       path: "women1",
-      img: "https://static.zara.net/photos///contents/mkt/spots/aw22-north-woman-new/subhome-xmedia-38-3//w/1922/IMAGE-landscape-default-fill-3826ba34-9fad-4264-9ecd-6d2bc9295d9c-default_0.jpg?ts=1663773455159",
+      img: "https://static.zara.net/assets/public/1c74/6ff4/64a74930be5c/0c2d1724a7ff/image-landscape-46d31401-b6cc-4ddd-ba6f-59743156450d-default_0/image-landscape-46d31401-b6cc-4ddd-ba6f-59743156450d-default_0.jpg?ts=1737365086986&w=1384",
     },
     {
       video: "https://res.cloudinary.com/dzsxh31vj/video/upload/v1737381505/jeans_mzvwwq.mp4",
@@ -28,7 +27,17 @@ const Homepage = () => {
   const Men = [
     {
       path: "men1",
-      img: "https://static.zara.net/photos///contents/mkt/spots/aw22-north-man-origins/subhome-xmedia-34//w/1294/IMAGE-landscape-1-fill-23c9012f-bdc9-45c1-af3c-8f7c07f86626-default_0.jpg?ts=1664366171420",
+      img: "https://static.zara.net/assets/public/a04f/7c52/7208479d90b3/405b1951ea68/image-landscape-default-fill-a4d13a6e-dbe6-4259-b2a9-ca2d6c155f52-default_0/image-landscape-default-fill-a4d13a6e-dbe6-4259-b2a9-ca2d6c155f52-default_0.jpg?ts=1736427762917&w=1384",
+    },
+    { path: "men2",
+      video: "blob:https://www.zara.com/cf821dd4-10f1-412c-885c-5bebb8a7d728",
+    },
+    {
+      path: "men2",
+      img: "https://static.zara.net/assets/public/4894/1c0a/4b78411c84c8/87ed0fc65938/image-landscape-default-fill-727a6739-cd4d-49a6-858a-181f8454ef44-default_0/image-landscape-default-fill-727a6739-cd4d-49a6-858a-181f8454ef44-default_0.jpg?ts=1736755109152&w=1384",
+    },
+    { path: "men3",
+      img: "https://static.zara.net/assets/public/9bc7/a9a5/319340e6906f/029055b12a04/image-landscape-default-fill-da20bbac-05ba-467c-83f9-396121a29f7b-default_0/image-landscape-default-fill-da20bbac-05ba-467c-83f9-396121a29f7b-default_0.jpg?ts=1736429615385&w=1384",
     },
     // ... (other Men items)
   ]
@@ -52,9 +61,6 @@ const Homepage = () => {
         direction="vertical"
         slidesPerView={1}
         mousewheel={true}
-        pagination={{
-          clickable: true,
-        }}
         onTouchMove={(e) => setActiveIndex(e.activeIndex === 0 ? 1 : e.activeIndex === 2 ? 2 : e.activeIndex)}
         className="w-full h-full"
         onScroll={(e) => setActiveIndex(e.activeIndex)}
@@ -72,11 +78,11 @@ const Homepage = () => {
                       : ""
                   } ${`main${category[indexNo]}${index}`}`}
                 />
-              ) : (
+              ) : ele.video ? (
                 <video autoPlay loop muted controls={ele.cat !== "kids"} className="w-full h-screen object-cover">
                   <source src={ele.video} type="video/mp4" />
                 </video>
-              )}
+              ) : null}
             </Link>
           </SwiperSlide>
         ))}
@@ -113,4 +119,3 @@ const Homepage = () => {
 }
 
 export default Homepage
-
