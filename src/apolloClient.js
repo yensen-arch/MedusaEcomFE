e92fde0d73441b81219b17b8fd1bca870e59bd1f
yensen-arch <officialdevain25@gmail.com>
2025-02-07 17:48:21 +0530

    saelor be integration

---
+++ src/apolloClient.js
@@ -0,0 +1,6 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://api.clothd.co/graphql/',
+  cache: new InMemoryCache(),
+});
--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/client';
+import { client } from './apolloClient';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
+    <ApolloProvider client={client}>
       <App />
+    </ApolloProvider>
   </BrowserRouter>
 );
 
