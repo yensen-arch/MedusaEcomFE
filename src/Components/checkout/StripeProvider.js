ff52e0be02f28ac6b03fbb8b8acb915e5018b1cd
yensen-arch <officialdevain25@gmail.com>
2025-03-09 20:46:51 +0530

    Live mode

--- src/Components/checkout/StripeProvider.js
+++ src/Components/checkout/StripeProvider.js
@@ -2,7 +2,7 @@
 import { loadStripe } from "@stripe/stripe-js";
 
 const stripePromise = loadStripe(
-  "pk_test_51QtM2fAotN9X1sy17CBqLAFeybXj9BbKu1wKY8IQhY5PcAwy4kQNM23XYcTinaTASJIJNEpzd82seY8sEMpSzk8b00pTVhX3Qp"
+  "pk_live_51QtM2fAotN9X1sy1UT2tPBi1t4qw6COv1egO1SAKoO1RnppBujn8R9P5IkJVwq9XzPMQCUT7CLVV6zogcubYV6UG00exB7bwNc"
 );
 
 export default stripePromise;
